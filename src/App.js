ac4ff942d54bdaadb0f8d125a23252dbb7f77ea4
hokhai846 <hokhai846@gmail.com>
2022-08-02 21:28:07 +0700

    hoan thanh he thong luoi Grid

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import Grid from './components/Grid';
+import { useEffect } from 'react';
+import Row from './components/Row';
+import Colum from './components/Colum';
+import classNames from 'classnames/bind';
+import styles from './App.module.scss';
+
+const cx = classNames.bind(styles);
 function App() {
-    return <div>React App</div>;
+    return (
+        <Grid grid>
+            <Row noGutter>
+                <Colum noGutter size={['l-4', 'm-6', 's-8']}>
+                    <div style={{ backgroundColor: 'red' }}>colum 1</div>
+                </Colum>
+                <Colum noGutter size={['l-4', 'm-6', 's-8']}>
+                    <div style={{ backgroundColor: 'red' }}>colum 1</div>
+                </Colum>
+                <Colum noGutter size={['l-4', 'm-6', 's-8']}>
+                    <div style={{ backgroundColor: 'red' }}>colum 1</div>
+                </Colum>
+                <Colum noGutter size={['l-4', 'm-6', 's-8']}>
+                    <div style={{ backgroundColor: 'red' }}>colum 1</div>
+                </Colum>
+                <Colum noGutter size={['l-4', 'm-6', 's-8']}>
+                    <div style={{ backgroundColor: 'red' }}>colum 1</div>
+                </Colum>
+                <Colum noGutter size={['l-4', 'm-6', 's-8']}>
+                    <div style={{ backgroundColor: 'red' }}>colum 1</div>
+                </Colum>
+            </Row>
+        </Grid>
+    );
 }
 
 export default App;
